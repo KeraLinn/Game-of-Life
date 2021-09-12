3aee888ee98bf00fef968acaa70114ed77edd4aa
KeraLinn <kera.linn@yahoo.com>
2021-09-12 18:00:47 -0500

    Added Height Labels, Width Lables, numericUpDowns for both.
    Did some resizing of dialog window, changed dialog name, moved the labels/buttons around within window

--- KL - Game of Life Program/ChangeSize Modal Dialog.Designer.cs
+++ KL - Game of Life Program/ChangeSize Modal Dialog.Designer.cs
@@ -31,12 +31,18 @@ namespace KL___Game_of_Life_Program
         {
             this.ChangeSizeOK = new System.Windows.Forms.Button();
             this.ChangeSizeCancel = new System.Windows.Forms.Button();
+            this.labelHeight = new System.Windows.Forms.Label();
+            this.labelWidth = new System.Windows.Forms.Label();
+            this.numericUpDownHeight = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownWidth = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownHeight)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownWidth)).BeginInit();
             this.SuspendLayout();
             // 
             // ChangeSizeOK
             // 
             this.ChangeSizeOK.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.ChangeSizeOK.Location = new System.Drawing.Point(110, 289);
+            this.ChangeSizeOK.Location = new System.Drawing.Point(57, 145);
             this.ChangeSizeOK.Name = "ChangeSizeOK";
             this.ChangeSizeOK.Size = new System.Drawing.Size(75, 23);
             this.ChangeSizeOK.TabIndex = 0;
@@ -46,12 +52,44 @@ namespace KL___Game_of_Life_Program
             // ChangeSizeCancel
             // 
             this.ChangeSizeCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.ChangeSizeCancel.Location = new System.Drawing.Point(253, 289);
+            this.ChangeSizeCancel.Location = new System.Drawing.Point(170, 145);
             this.ChangeSizeCancel.Name = "ChangeSizeCancel";
             this.ChangeSizeCancel.Size = new System.Drawing.Size(75, 23);
             this.ChangeSizeCancel.TabIndex = 1;
             this.ChangeSizeCancel.Text = "Cancel";
             this.ChangeSizeCancel.UseVisualStyleBackColor = true;
+            // 
+            // labelHeight
+            // 
+            this.labelHeight.AutoSize = true;
+            this.labelHeight.Location = new System.Drawing.Point(86, 38);
+            this.labelHeight.Name = "labelHeight";
+            this.labelHeight.Size = new System.Drawing.Size(38, 13);
+            this.labelHeight.TabIndex = 2;
+            this.labelHeight.Text = "Height";
+            // 
+            // labelWidth
+            // 
+            this.labelWidth.AutoSize = true;
+            this.labelWidth.Location = new System.Drawing.Point(86, 87);
+            this.labelWidth.Name = "labelWidth";
+            this.labelWidth.Size = new System.Drawing.Size(35, 13);
+            this.labelWidth.TabIndex = 3;
+            this.labelWidth.Text = "Width";
+            // 
+            // numericUpDownHeight
+            // 
+            this.numericUpDownHeight.Location = new System.Drawing.Point(145, 36);
+            this.numericUpDownHeight.Name = "numericUpDownHeight";
+            this.numericUpDownHeight.Size = new System.Drawing.Size(72, 20);
+            this.numericUpDownHeight.TabIndex = 4;
+            // 
+            // numericUpDownWidth
+            // 
+            this.numericUpDownWidth.Location = new System.Drawing.Point(145, 85);
+            this.numericUpDownWidth.Name = "numericUpDownWidth";
+            this.numericUpDownWidth.Size = new System.Drawing.Size(72, 20);
+            this.numericUpDownWidth.TabIndex = 6;
             // 
             // ChangeSize_Modal_Dialog
             // 
@@ -59,7 +97,11 @@ namespace KL___Game_of_Life_Program
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.ChangeSizeCancel;
-            this.ClientSize = new System.Drawing.Size(460, 361);
+            this.ClientSize = new System.Drawing.Size(338, 212);
+            this.Controls.Add(this.numericUpDownWidth);
+            this.Controls.Add(this.numericUpDownHeight);
+            this.Controls.Add(this.labelWidth);
+            this.Controls.Add(this.labelHeight);
             this.Controls.Add(this.ChangeSizeCancel);
             this.Controls.Add(this.ChangeSizeOK);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
@@ -67,8 +109,11 @@ namespace KL___Game_of_Life_Program
             this.MinimizeBox = false;
             this.Name = "ChangeSize_Modal_Dialog";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "ChangeSize_Modal_Dialog";
+            this.Text = "Set Universe Size";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownHeight)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownWidth)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -76,5 +121,9 @@ namespace KL___Game_of_Life_Program
 
         private System.Windows.Forms.Button ChangeSizeOK;
         private System.Windows.Forms.Button ChangeSizeCancel;
+        private System.Windows.Forms.Label labelHeight;
+        private System.Windows.Forms.Label labelWidth;
+        private System.Windows.Forms.NumericUpDown numericUpDownHeight;
+        private System.Windows.Forms.NumericUpDown numericUpDownWidth;
     }
 }
