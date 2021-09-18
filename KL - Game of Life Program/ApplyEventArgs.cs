c209e316171be000f1a870aaae790c6ed450e8e1
KeraLinn <kera.linn@yahoo.com>
2021-09-18 14:33:38 -0500

    Added ApplyEventArgs class for Randomize from Seed, by following modal dialog tutorial but I don't think I actually need it for seed?

---
+++ KL - Game of Life Program/ApplyEventArgs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KL___Game_of_Life_Program
+{
+    public class ApplyEventArgs : EventArgs
+    {
+        int seed;
+
+        public int Seed
+        {
+            get { return seed; }
+            set { seed = value; }
+        }
+        public ApplyEventArgs(int seed)
+        {
+            this.seed = seed;
+        }
+    }
+}
