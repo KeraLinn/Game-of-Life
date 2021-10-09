3ec44099c195abe024647c088a696d9b908e68ea
KeraLinn <kera.linn@yahoo.com>
2021-10-09 15:39:02 -0500

    Removed creating new rectangle for neighborcount, figured out just have to put into cellRect instead.

--- KL - Game of Life Program/Form1.cs
+++ KL - Game of Life Program/Form1.cs
@@ -196,9 +196,9 @@ namespace KL___Game_of_Life_Program
 						e.Graphics.FillRectangle(cellBrush, cellRect);
 
 						//---poss here to add the bit about neighborct. bc it only needs a ct if it's alive?
-						Rectangle rect = new Rectangle(0, 0, 100, 100);
+						//dont think need new rect actually, should jsut put in cellRect?  Rectangle rect = new Rectangle(0, 0, 100, 100);
 						int neighbors = 8;
-						e.Graphics.DrawString(neighbors.ToString(), font, Brushes.Black, rect, stringFormat);
+						e.Graphics.DrawString(neighbors.ToString(), font, Brushes.Black, cellRect, stringFormat);
 					}
 					// Outline the cell with a pen
 					e.Graphics.DrawRectangle(gridPen, cellRect.X, cellRect.Y, cellRect.Width, cellRect.Height);
